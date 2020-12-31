1fb4786faaccb2b799aee5921a5dff30bd38efc4
Abhijeet Satpute <abhi111sat@gmail.com>
2020-12-31 17:24:54 +0530

    Create 2. Array insert at index.cpp

---
+++ GFG/2.Arrays/2. Array insert at index.cpp
@@ -0,0 +1,66 @@
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertAtIndex(int arr[],int sizeOfArray,int index,int element);
+
+ // } Driver Code Ends
+
+
+
+// You need to insert the given element at the given index.
+// After inserting the elements at index, elements
+// from index onward should be shifted one position ahead
+// You may assume that the array already has sizeOfArray - 1
+// elements.
+void insertAtIndex(int arr[], int sizeOfArray, int index, int element)
+{
+    //Your code here
+    for(int i=sizeOfArray;i>=index;i--)
+    {
+        arr[i] = arr[i-1];
+    }
+    arr[index] = element;
+}
+
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;//Input testcases
+    while(t--)
+    {
+
+        int sizeOfArray;
+
+        //input size of Array
+        cin>>sizeOfArray;
+
+        int arr[sizeOfArray];
+
+        //input elements in the Array
+        for(int i=0;i<sizeOfArray-1;i++)
+        cin>>arr[i];
+
+        int index;
+        //input index
+        cin>>index;
+        int element;
+        //input element
+        cin>>element;
+
+        //calling insertAtIndex() function
+        insertAtIndex(arr,sizeOfArray,index,element); //Complete this function
+
+        for(int i=0;i<sizeOfArray;i++)//Printing the array for verification
+        cout<<arr[i]<<" ";
+
+        cout<<endl;
+
+    }
+
+	return 0;
+}  // } Driver Code Ends
